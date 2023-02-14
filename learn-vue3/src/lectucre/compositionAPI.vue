6244b125e7191ab5a7bec09f5a0c83fc7f46080d
greenT-Hee <heehee970322@gmail.com>
2023-02-14 16:25:03 +0900

    :hammer: Add:템플릿 문법

---
+++ learn-vue3/src/lectucre/compositionAPI.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<h2>반응형</h2>
+		<p>{{ reactiveMessage }}</p>
+		<button v-on:click="addReactiveMsg">Add message</button>
+		<h2>일반</h2>
+		<p>{{ normalMesssage }}</p>
+		<button v-on:click="addNoramlMsg">Add message</button>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		// 반응형 상태 선언
+		const reactiveMessage = ref('reactive message 🐰');
+		const addReactiveMsg = () => {
+			reactiveMessage.value += '🥕';
+		};
+
+		// 일반 변수 선언
+		const normalMesssage = 'normal message 🫥';
+		const addNoramlMsg = () => {
+			// normalMesssage = normalMesssage + '!';  반응형이 아님 (error)
+		};
+
+		return {
+			reactiveMessage,
+			normalMesssage,
+			addReactiveMsg,
+			addNoramlMsg,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ learn-vue3/src/lectucre/templeteApp.vue
@@ -0,0 +1,44 @@
+<template>
+	<h2>보간법</h2>
+	<p>{{ message }}</p>
+	<p v-once>{{ message }}</p>
+	<button v-on:click="message += '🦖'">추가 버튼</button>
+	<hr />
+	<h2>v-html</h2>
+	<p v-html="rawHTML"></p>
+	<hr />
+	<h2>속성 바인딩</h2>
+	<div :title="dynamicTitle">마우스를 올려보세요</div>
+	<input type="text" value="홍길동" :disabled="isDisabled" />
+	<input v-bind="attrs" />
+	<hr />
+	<h2>JavaScript</h2>
+	{{ message.split('').reverse().join('') }}
+	{{ isDisabled ? 'NO' : 'YES' }}
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+	setup() {
+		const message = ref('hello');
+		const rawHTML = ref('<strong>안녕하세요</strong>');
+		const dynamicTitle = ref('안녕하세요!!!!!!!!!!');
+		const isDisabled = ref(true);
+		const attrs = ref({
+			type: 'password',
+			value: '1234',
+			disabled: false,
+		});
+		return {
+			message,
+			rawHTML,
+			dynamicTitle,
+			isDisabled,
+			attrs,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
